e8ec7c7b8878bd8f229e9a48595abe3920de4ce5
Riko Ukena <riko.ukena@tuhh.de>
2021-10-28 16:15:42 +0200

    ...

--- game/forms.py
+++ game/forms.py
@@ -47,7 +47,7 @@ class MatchSearchForm(forms.Form):
         initial=True)
     maps = ChoiceField(widget=forms.Select(attrs={'onchange':'this.form.submit()'}),
         # choices=get_relevant_maps(Map.objects.all()), 
-        choices == [("All","All Maps")],
+        choices = [("All","All Maps")],
         label= "Map:")
     durationrange = ChoiceField(widget=forms.Select(attrs={'onchange':'this.form.submit()'}),
         choices = [("All","All Durations"),
