7f21c2da8db147d213ff19fd1732314be6ee27ca
Riko Ukena <riko.ukena@tuhh.de>
2021-10-28 22:03:34 +0200

    fix media?

--- aoematchups/settings.py
+++ aoematchups/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Media
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 LOGGING = {
     'version': 1,
